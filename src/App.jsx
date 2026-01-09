43fa2f2aa64a19cbcbce1add5fd0c478dad9b54d
Turner Inge <tinge@villanova.edu.>
2026-01-09 10:41:26 -0500

    Added supabaseClient to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-
+import { useEffect, useState } from 'react';
+import { supabase } from './config/supabaseClient'; // Adjust path if needed
 function App() {
   const [count, setCount] = useState(0)
 
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Hello Turner + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
