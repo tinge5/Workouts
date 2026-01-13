3f0b4b0fe0d9090c1f6c88df095ddc19d0824719
Turner Inge <tinge@villanova.edu.>
2026-01-13 16:24:30 -0500

    Added signin button

---
+++ src/Pages/Auth.jsx
@@ -0,0 +1,10 @@
+import {supabase} from '../config/supabaseClient.js';
+
+export default function Auth() {
+  return (
+    <div>
+      <h1>Auth Page</h1>
+      <p>This is the authentication page of the application.</p>
+    </div>
+  );
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import './css/Home.css';
 export default function Home() {
   const navigate = useNavigate();
+
   return (
-    <img src ="../public/images/workoutslogo.svg" alt="Workouts Logo" className="workouts-logo" onClick={() => navigate('/plans')} />
+    <div className='screen'>
+      <img src ="../public/images/workoutslogo.svg" alt="Workouts Logo" className="workouts-logo"></img>
+      <button className="buttons" onClick={() => navigate('/auth')}>Sign in</button>     
+    </div>
   );
 }
