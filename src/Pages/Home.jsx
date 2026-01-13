fda759242bc5b02697b67356d4eb1c653f311d5a
Turner Inge <tinge@villanova.edu.>
2026-01-12 22:40:25 -0500

    Added logo and start page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,9 @@
 
 import { useNavigate } from 'react-router-dom';
+import './css/Home.css';
 export default function Home() {
   const navigate = useNavigate();
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-
-      <p>
-        This is the home page of the application.
-    </p>
-    <button onClick={() => navigate('/plans')}>Go to Plans</button>
-    </div>
-    
+    <img src ="../public/images/workoutslogo.svg" alt="Workouts Logo" className="workouts-logo" onClick={() => navigate('/plans')} />
   );
 }
