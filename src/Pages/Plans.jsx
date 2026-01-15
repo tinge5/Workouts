799be44c94b4e7bf18a0b77edb10de9381b281e4
Turner Inge <tinge@villanova.edu.>
2026-01-15 15:04:00 -0500

    Added workout cards and glow to them

--- src/Pages/Plans.jsx
+++ src/Pages/Plans.jsx
@@ -7,7 +7,15 @@ export default function Plans() {
       <h1 style={{ color: "white", textAlign: "center" }}>Plans Page</h1>
 
       <div className="plans-card">
-      <span>This is the plans page of the application. I willcontinue to spit fdlasfjlsadjfkslfsajfl;jfd;asjf;safjs;afjlksa;jfkls;ajfsadjf;</span>
+
+      <span className="cards">This is the plans page of the application. I willcontinue to spit fdlasfjlsadjfkslfsajfl;jfd;asjf;safjs;afjlksa;jfkls;ajfsadjf;</span>
+      <span className="cards">This is the plans page of the application. I willcontinue to spit fdlasfjlsadjfkslfsajfl;jfd;asjf;safjs;afjlksa;jfkls;ajfsadjf;</span>
+      <span className="cards">This is the plans page of the application. I willcontinue to spit fdlasfjlsadjfkslfsajfl;jfd;asjf;safjs;afjlksa;jfkls;ajfsadjf;</span>
+      <span className="cards">This is the plans page of the application. I willcontinue to spit fdlasfjlsadjfkslfsajfl;jfd;asjf;safjs;afjlksa;jfkls;ajfsadjf;</span>
+      <span className="cards">This is the plans page of the application. I willcontinue to spit fdlasfjlsadjfkslfsajfl;jfd;asjf;safjs;afjlksa;jfkls;ajfsadjf;</span>
+      <span className="cards">This is the plans page of the application. I willcontinue to spit fdlasfjlsadjfkslfsajfl;jfd;asjf;safjs;afjlksa;jfkls;ajfsadjf;</span>
+      <span className="cards">This is the plans page of the application. I willcontinue to spit fdlasfjlsadjfkslfsajfl;jfd;asjf;safjs;afjlksa;jfkls;ajfsadjf;</span>
+
       </div>
     </div>
   );
