95a97c2fb9f606bd612828ee8a908d24dbb07051
Turner Inge <tinge@villanova.edu.>
2026-01-15 19:24:28 -0500

    testing mobile abilities

--- src/BodyClassController.jsx
+++ src/BodyClassController.jsx
@@ -7,15 +7,23 @@ export default function BodyBackgroundController() {
 
   useEffect(() => {
     const body = document.body;
+    const metaTheme = document.querySelector('meta[name="theme-color"]');
 
     // Default background
     body.style.backgroundColor = "#101010";
     document.documentElement.style.backgroundColor = "#101010";
+          if (metaTheme) {
+        metaTheme.setAttribute('content', '#101010'); // change to red
+      }
+
 
     if (location.pathname === "/plans") {
       console.log("Plans page");
     }
     if (location.pathname === "/auth") {
+      if (metaTheme) {
+        metaTheme.setAttribute('content', 'gray'); // change to red
+      }
       body.style.backgroundColor = "gray"; 
       document.documentElement.style.backgroundColor = "gray";
       console.log("Auth page");
