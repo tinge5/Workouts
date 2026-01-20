77a071c623f9134e6b15ceeb11c23bd966fa38ee
Turner Inge <tinge@villanova.edu.>
2026-01-20 16:33:40 -0500

    logo takes you to plans

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   const user1 = useRStatus();
   return (
     <div className="header">
-      <img src="../images/workoutslogo.svg" alt="Logo" className="logo" />
+      <img src="../images/workoutslogo.svg" alt="Logo" className="logo" onClick={() => navigate('/plans')}/>
       {user1 ? <img src="../images/Profile.svg" alt="Profile" className="profile-logo" onClick={() => navigate('/profile')}/> : null}
     </div>
   );
